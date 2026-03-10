3f28190523fb39772aa67f1047653f05ae9ff167
Christopher Lang <christopher.lang.256@gmail.com>
2026-03-10 10:58:19 +0000

    Cleanup promising test

--- ArchsemLean/Common.lean
+++ ArchsemLean/Common.lean
@@ -7,11 +7,32 @@ open Sail.ArchSem
 /-
  - CR clang for leo: we should modify the Arch interface and sail-lean backend to
  - have this typeclass instance in Arch.
+ - It would be nice to have toString instances for registers and register types.
  -/
 instance : BEq (Arch.register_type ._PC) := by
   have eq : Arch.register_type = RegisterType := rfl
   rw [eq]
   infer_instance
+instance (reg : Register) : Inhabited (Arch.register_type reg) := by
+  have eq : Arch.register_type = RegisterType := rfl
+  rw [eq]
+  rw [RegisterType.eq_def]
+  split <;> infer_instance
+instance (reg : Register) : Repr (Arch.register_type reg) := by
+  have eq : Arch.register_type = RegisterType := rfl
+  rw [eq]
+  rw [RegisterType.eq_def]
+  split <;> infer_instance
+
+/-
+CR clang for leo: I'm amazed lean does not have this in core libs?!
+I need it for guarding test results.
+-/
+instance [BEq α] [BEq β] : BEq (Except α β) where
+  beq
+    | .error e1, .error e2 => e1 == e2
+    | .ok v1, .ok v2 => v1 == v2
+    | _, _ => false
 
 /-
  - If we are in a context where the number of threads is known then we prefer
@@ -23,3 +44,5 @@ abbrev Tid := Nat
 
 abbrev RegisterMap [Arch] := Std.ExtDHashMap Arch.register Arch.register_type
 abbrev TerminationCondition [Arch] (nThreads : Nat) := Fin nThreads → RegisterMap → Bool
+
+def RegisterMap.empty : RegisterMap := Std.ExtDHashMap.emptyWithCapacity 64
--- ArchsemLean/PromisingTest.lean
+++ ArchsemLean/PromisingTest.lean
@@ -7,10 +7,43 @@ open ArchSem.Promising
 
 /- CR clang: there is some duplication here with sequentialtests. -/
 
+def isem : SailM Unit := Out.Functions.fetch_and_execute ()
+
+def memInsert4 (addr : BitVec 64) (value : BitVec 32) (mem : InitialMem) :=
+  assert! (addr.getLsb 0) == false
+  assert! (addr.getLsb 1) == false
+  if addr.getLsb 2 then /- Upper half. -/
+    let word : BitVec 64 := mem.getD (addr >>> 3) (BitVec.zero 64)
+    let word := word &&& (BitVec.ofInt 64 (0xFFFFFFFF <<< 32))
+    let word := word ||| (value <<< 32)
+    mem.insert (addr >>> 3) word
+  else /- Lower half. -/
+    let word := mem.getD (addr >>> 3) (BitVec.zero 64)
+    let word := word &&& (BitVec.ofInt 64 0xFFFFFFFF)
+    let word := word ||| value
+    mem.insert (addr >>> 3) word
+
+def extractRegs (regs : List Register) (ts : ThreadState) : String :=
+  let getRegValue (reg : Register) : Arch.register_type reg :=
+    ts.regs.getD reg (default, 0) |> Prod.fst
+  let strings := regs.map (fun reg =>
+    let value := getRegValue reg
+    (reprStr reg) ++ "=" ++ (reprStr value))
+  " ".intercalate strings
+
+def prepareTestResults (extractor : ThreadState → String)
+    (res : ExecutionMonad.NResult (ModelState n × String) (ModelState n × ModelState n) )
+    : List (Except String (List String)) :=
+  let errs := res.errors.map (Except.error ∘ Prod.snd)
+  let finalStates := res.results.map (Vector.toList ∘ ModelState.threadStates ∘ Prod.snd)
+  let finalStatesString := finalStates.map (fun threads => threads.map extractor)
+  let results := finalStatesString.map Except.ok
+  results ++ errs
+
 section EOR
 
-def initialRegs : Std.ExtDHashMap Arch.register Arch.register_type :=
-  (Std.ExtDHashMap.emptyWithCapacity 64)
+def initialRegs : RegisterMap :=
+  RegisterMap.empty
   |>.insert ._PC (BitVec.ofNat 64 0x500)
   |>.insert .R0 (BitVec.ofNat 64 0x0)
   |>.insert .R1 (BitVec.ofNat 64 0x11)
@@ -18,8 +51,7 @@ def initialRegs : Std.ExtDHashMap Arch.register Arch.register_type :=
 
 def initialMem : InitialMem :=
   (Std.ExtHashMap.emptyWithCapacity 32)
-  /- CR clang: write a mem_insert helper. -/
-  |>.insert (0x500 >>> 3) 0xca020020
+  |> memInsert4 0x500 0xca020020
 
 def initialState : ModelState 1 := {
   threadStates := ⟨#[
@@ -32,20 +64,11 @@ def initialState : ModelState 1 := {
 def terminationCondition : TerminationCondition 1 := fun _tid regs =>
   regs.get? ._PC == .some (BitVec.ofNat 64 0x504)
 
-def isem : SailM Unit := Out.Functions.fetch_and_execute ()
 def fuel := 1
 
-def outputTState (tstate : ThreadState) : String :=
-  let r0 : Option (BitVec 64) := (tstate.regs.get? .R0).map (Prod.fst)
-  toString r0
+def testResults := runPromiseFirst fuel isem terminationCondition initialState
 
-def testOutput : String :=
-  let res := runPromiseFirst fuel isem terminationCondition initialState
-  match res.errors with
-  | [] => "output:\n" ++ "\n".intercalate
-    ((res.results.map Prod.snd).map (fun pstate => outputTState pstate.threadStates[0]))
-  | errs => "errors:\n" ++ "\n".intercalate (errs.map Prod.snd)
-
-#eval testOutput
+#guard prepareTestResults (extractRegs [.R0]) testResults ==
+  [Except.ok ["Register.R0=0x0000000000000110#64"]]
 
 end EOR
