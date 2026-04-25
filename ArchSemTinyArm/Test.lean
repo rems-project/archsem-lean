fefb80f0f955b89eaf54ac512c5e3aa44bec3b8f
Christopher Lang <christopher.lang.256@gmail.com>
2026-04-25 13:20:52 +0000

    Add promise first regression litmus tests

--- ArchSemTinyArm/Test.lean
+++ ArchSemTinyArm/Test.lean
@@ -13,7 +13,7 @@ This file contains regression tests for all TinyArm concurrency models.
 
 namespace ArchSemTinyArm.Promising.Test
 
-def fuel : Nat := 10
+def fuel : Nat := 100
 def isem : SailM Unit := sailTinyArmIsem
 
 def naiveModel : ComputationalTerminatingModel :=
@@ -21,10 +21,30 @@ def naiveModel : ComputationalTerminatingModel :=
 def promiseFirstModel : ComputationalTerminatingModel :=
   Promising.createPromiseFirstModel isem fuel
 
-#eval guardTestFromFile true naiveModel "litmus_tests/MP.archsem.toml"
-#eval guardTestFromFile true naiveModel "litmus_tests/MP+dmbs.archsem.toml"
+def regressionLitmusTests : List (Bool × String) := [
+  (false, "./litmus_tests/arm-small/SB+dmb.sys.archsem.toml"),
+  (false, "./litmus_tests/arm-small/R+dmb.sys.archsem.toml"),
+  (true, "./litmus_tests/arm-small/2+2W.archsem.toml"),
+  (true, "./litmus_tests/arm-small/SB+dmb.sy+po.archsem.toml"),
+  (true, "./litmus_tests/arm-small/S.archsem.toml"),
+  (false, "./litmus_tests/arm-small/MP+dmb.sys.archsem.toml"),
+  (false, "./litmus_tests/arm-small/2+2W+dmb.sys.archsem.toml"),
+  (true, "./litmus_tests/arm-small/R+po+dmb.sy.archsem.toml"),
+  (true, "./litmus_tests/arm-small/MP.archsem.toml"),
+  (true, "./litmus_tests/arm-small/S+po+dmb.sy.archsem.toml"),
+  (true, "./litmus_tests/arm-small/LB+dmb.sy+po.archsem.toml"),
+  (true, "./litmus_tests/arm-small/MP+po+dmb.sy.archsem.toml"),
+  (true, "./litmus_tests/arm-small/SB.archsem.toml"),
+  (true, "./litmus_tests/arm-small/R+dmb.sy+po.archsem.toml"),
+  (true, "./litmus_tests/arm-small/LB.archsem.toml"),
+  (true, "./litmus_tests/arm-small/MP+dmb.sy+po.archsem.toml"),
+  (true, "./litmus_tests/arm-small/R.archsem.toml"),
+  (true, "./litmus_tests/arm-small/S+dmb.sy+po.archsem.toml"),
+  (false, "./litmus_tests/arm-small/S+dmb.sys.archsem.toml"),
+  (true, "./litmus_tests/arm-small/2+2W+dmb.sy+po.archsem.toml"),
+  (false, "./litmus_tests/arm-small/LB+dmb.sys.archsem.toml"),
+]
 
-#eval guardTestFromFile true promiseFirstModel "litmus_tests/MP.archsem.toml"
-#eval guardTestFromFile true promiseFirstModel "litmus_tests/MP+dmbs.archsem.toml"
+#eval regressionLitmusTests.forM (fun (allowed,fname)=> guardTestFromFile allowed promiseFirstModel fname)
 
 end ArchSemTinyArm.Promising.Test
