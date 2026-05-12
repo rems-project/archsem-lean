fee1d08e51c428471c395450e8ae19ad15d929ce
Christopher Lang <christopher.lang.256@gmail.com>
2026-05-12 16:13:01 +0000

    Add MPDMBS promising test

--- ArchSemTinyArm/PromisingTest.lean
+++ ArchSemTinyArm/PromisingTest.lean
@@ -172,4 +172,90 @@ def promiseFirstResults := prepareTestResults finalStateExtractor promiseFirstOu
 
 end MP
 
+namespace MPDMBS
+/-
+The classic MP litmus test with memory barriers.
+Thread 1: STR X2, [X1, X0]; DMB SY; STR X5, [X4, X3]
+Thread 2: LDR X5, [X4, X3]; DMB SY; LDR X2, [X1, X0]
+
+
+Expected outcome of (R5, R2) at Thread 2:
+  (0x1, 0x2a), (0x0, 0x2a), (0x0, 0x0)
+-/
+
+def initialRegsT0 : RegisterMap :=
+  RegisterMap.empty
+  |>.insert ._PC (BitVec.ofNat 64 0x500)
+  |>.insert .R0 (BitVec.ofNat 64 0x1000)
+  |>.insert .R1 (BitVec.ofNat 64 0x100)
+  |>.insert .R2 (BitVec.ofNat 64 0x2a)
+  |>.insert .R3 (BitVec.ofNat 64 0x1000)
+  |>.insert .R4 (BitVec.ofNat 64 0x200)
+  |>.insert .R5 (BitVec.ofNat 64 0x1)
+
+def initialRegsT1 : RegisterMap :=
+  RegisterMap.empty
+  |>.insert ._PC (BitVec.ofNat 64 0x600)
+  |>.insert .R0 (BitVec.ofNat 64 0x1000)
+  |>.insert .R1 (BitVec.ofNat 64 0x100)
+  |>.insert .R2 (BitVec.ofNat 64 0x0)
+  |>.insert .R3 (BitVec.ofNat 64 0x1000)
+  |>.insert .R4 (BitVec.ofNat 64 0x200)
+  |>.insert .R5 (BitVec.ofNat 64 0x0)
+
+def initialMem : MemoryMap :=
+  MemoryMap.empty
+  /- Thread 0 @ 0x500 -/
+  |>.write 4 0x500 0xf8206822  /- STR X2, [X1, X0] -/
+  |>.write 4 0x504 0xd5033fbf  /- DMB SY -/
+  |>.write 4 0x508 0xf8236885  /- STR X5, [X4, X3] -/
+  /- Thread 1 @ 0x600 -/
+  |>.write 4 0x600 0xf8636885  /- LDR X5, [X4, X3] -/
+  |>.write 4 0x604 0xd5033fbf  /- DMB SY -/
+  |>.write 4 0x608 0xf8606822  /- LDR X2, [X1, X0] -/
+  /- Backing memory so the addresses exist -/
+  |>.write 8 0x1100 0x0
+  |>.write 8 0x1200 0x0
+
+abbrev nThreads := 2
+def initialState : ArchState nThreads := {
+  regs := ⟨#[
+    initialRegsT0,
+    initialRegsT1
+  ], rfl⟩,
+  memory := initialMem,
+  addressSpace := ()
+}
+
+def terminationCondition : TerminationCondition nThreads := fun tid regs =>
+  match tid with
+  | 0 => regs.get? ._PC == .some (BitVec.ofNat 64 0x50c)
+  | 1 => regs.get? ._PC == .some (BitVec.ofNat 64 0x60c)
+
+def fuel := 9
+def finalStateExtractor : ArchState nThreads → List (Option String)
+  := extractRegs [(1, .R5), (1, .R2)]
+def expectedResults := [
+  [some "0x0000000000000001#64", some "0x000000000000002a#64"],
+  [some "0x0000000000000000#64", some "0x000000000000002a#64"],
+  [some "0x0000000000000000#64", some "0x0000000000000000#64"]]
+
+def naiveModel : ComputationalTerminatingModel := createNaiveModel isem fuel
+def promiseFirstModel : ComputationalTerminatingModel := createPromiseFirstModel isem fuel
+
+def naiveOutput := naiveModel nThreads terminationCondition initialState
+def naiveResults := prepareTestResults finalStateExtractor naiveOutput
+def promiseFirstOutput := promiseFirstModel nThreads terminationCondition initialState
+def promiseFirstResults := prepareTestResults finalStateExtractor promiseFirstOutput
+
+-- Check no errors
+#guard naiveResults.fst == []
+#guard promiseFirstResults.fst == []
+
+-- Check expected results
+#guard naiveResults.snd.isPerm expectedResults
+#guard promiseFirstResults.snd.isPerm expectedResults
+
+end MPDMBS
+
 end ArchSemTinyArm.PromisingTest
